708056c3840a60144ff1114516bf9b0dfc0ff57c
RSamaium <sam@newbot.io>
2023-09-11 15:14:56 +0200

    user card

--- src/app/layout/users/users.module.ts
+++ src/app/layout/users/users.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { UsersComponent } from './users.component'
 import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { UserCardComponent } from './user-card/user-card.component'
 
 @NgModule({
-    declarations: [UsersComponent],
+    declarations: [UsersComponent, UserCardComponent],
     imports: [
         CommonModule,
         FormsModule
