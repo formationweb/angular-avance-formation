f843dfe623691154c9cdee9380d13666ef9bbb43
RSamaium <sam@newbot.io>
2023-09-12 10:02:14 +0200

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { BASE_URL } from './app/core/constants/injection';
+import { environment } from './environments/environment';
+
 
 platformBrowserDynamic([
   {
     provide: BASE_URL,
-    useValue: 'https://jsonplaceholder.typicode.com'
+    useValue: environment.baseUrl
   }
 ]).bootstrapModule(AppModule)
   .catch(err => console.error(err));
