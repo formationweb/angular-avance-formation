041ab6f64d30f74d3aa04afda3b8a59fafd2fae0
RSamaium <sam@newbot.io>
2024-05-13 11:24:21 +0200

    user service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,1 +1,19 @@
-// https://jsonplaceholder.typicode.com/users/1
+// https://jsonplaceholder.typicode.com/users
+
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "../interfaces/user.interface";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    readonly url = 'https://jsonplaceholder.typicode.com/users'
+    private http = inject(HttpClient)
+   // constructor(private http: HttpClient) {}
+
+    getAll(): Observable<User[]> {
+        return this.http.get<User[]>(this.url)
+    }
+}
