6e49d46a624c5fe9ff447e1f545204ce610ce669
RSamaium <sam@newbot.io>
2024-07-22 10:43:57 +0200

    users component

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,1 +1,18 @@
-// https://jsonplaceholder.typicode.com/users
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private http = inject(HttpClient);
+  readonly url = 'https://jsonplaceholder.typicode.com/users';
+
+  //constructor(private http: HttpClient) {}
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.url);
+  }
+}
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { User } from '../../core/interfaces/user.interface';
+import { UserService } from '../../core/services/user.service';
 import { NavbarComponent } from '../../features/navbar/navbar.component';
 
 @Component({
@@ -6,8 +8,15 @@ import { NavbarComponent } from '../../features/navbar/navbar.component';
   standalone: true,
   imports: [NavbarComponent],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  private userService = inject(UserService);
+  users: User[] = [];
 
+  ngOnInit(): void {
+    this.userService.getAll().subscribe((users) => {
+      this.users = users;
+    });
+  }
 }
